338ed7a87a9ad97a098acf573d33f636d9490c3e
mike.zhang <mike.zhang@yeamoney.cn>
2016-11-30 14:47:11 +0800

    一个宏大的命题是写代码能够做些什么？
    一个宏大的命题是写代码能够做些什么？

---
+++ app.js
@@ -0,0 +1,95 @@
+var port = (process.env.VCAP_APP_PORT || 3000);
+var express = require("express");
+
+var app = express();
+var sentiment = require('sentiment');
+var tweetCount = 0;
+var tweetTotalSentiment = 0;
+var monitoringPhrase;
+
+function resetMonitoring() {
+    monitoringPhrase = "";
+}
+
+function beginMonitoring(phrase) {
+    var stream;
+    // cleanup if we're re-setting the monitoring
+    if (monitoringPhrase) {
+        resetMonitoring();
+    }
+    monitoringPhrase = phrase;
+    tweetCount = 0;
+    tweetTotalSentiment = 0;
+    tweeter.verifyCredentials(function (error, data) {
+        if (error) {
+            return "Error connecting to Twitter: " + error;
+        } else {
+            stream = tweeter.stream('statuses/filter', {
+                'track': monitoringPhrase
+            }, function (stream) {
+                console.log("Monitoring Twitter for " + monitoringPhrase);
+                stream.on('data', function (data) {
+                    // only evaluate the sentiment of English-language tweets
+                    if (data.lang === 'en') {
+                        sentiment(data.text, function (err, result) {
+                            tweetCount++;
+                            tweetTotalSentiment += result.score;
+                        });
+                    }
+                });
+            });
+            return stream;
+        }
+    });
+}
+
+function sentimentImage() {
+    var avg = tweetTotalSentiment / tweetCount;
+    if (avg > 0.5) { // happy
+        return "/images/excited.png";
+    }
+    if (avg < -0.5) { // angry
+        return "/images/angry.png";
+    }
+    // neutral
+    return "/images/content.png";
+}
+
+app.get('/',
+    function (req, res) {
+        var welcomeResponse = "<HEAD>" +
+            "<title>Twitter Sentiment Analysis</title>\n" +
+            "</HEAD>\n" +
+            "<BODY>\n" +
+            "<P>\n" +
+            "Welcome to the Twitter Sentiment Analysis app.<br>\n" +
+            "What would you like to monitor?\n" +
+            "</P>\n" +
+            "<FORM action=\"/monitor\" method=\"get\">\n" +
+            "<P>\n" +
+            "<INPUT type=\"text\" name=\"phrase\"><br><br>\n" +
+            "<INPUT type=\"submit\" value=\"Go\">\n" +
+            "</P>\n" + "</FORM>\n" + "</BODY>";
+        if (!monitoringPhrase) {
+            res.send(welcomeResponse);
+        } else {
+            var monitoringResponse = "<HEAD>" +
+                "<META http-equiv=\"refresh\" content=\"5; URL=http://" +
+                req.headers.host +
+                "/\">\n" +
+                "<title>Twitter Sentiment Analysis</title>\n" +
+                "</HEAD>\n" +
+                "<BODY>\n" +
+                "<P>\n" +
+                "The Twittersphere is feeling<br>\n" +
+                "<IMG align=\"middle\" src=\"" + sentimentImage() + "\"/><br>\n" +
+                "about " + monitoringPhrase + ".<br><br>" +
+                "Analyzed " + tweetCount + " tweets...<br>" +
+                "</P>\n" +
+                "<A href=\"/reset\">Monitor another phrase</A>\n" +
+                "</BODY>";
+            res.send(monitoringResponse);
+        }
+    });
+app.listen(port);
+console.log("Server listening on port " + port);
